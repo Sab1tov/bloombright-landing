c2a7b127f8d9a7babd9d8c27f264df4978d7458c
Sab1tov <bestplayer0202@gmail.com>
2025-08-11 22:31:04 +0500

    final fixes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
 	experimental: {
 		optimizePackageImports: ['lucide-react'],
 	},
+	eslint: {
+		ignoreDuringBuilds: false,
+	},
 	images: {
 		formats: ['image/webp', 'image/avif'],
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
